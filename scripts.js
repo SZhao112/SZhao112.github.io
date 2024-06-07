b36ac8d73fcf4ba28292a2a5342b1c1fadb41a40
SZhao112 <szhao112@uottawa.ca>
2024-06-08 06:08:00 +0800

    index service finished

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navbarToggler = document.querySelector(".navbar-toggler");
+    const navbarCollapse = document.querySelector("#navbarNav");
+
+    if (navbarToggler) {
+        navbarToggler.addEventListener("click", function () {
+            navbarCollapse.classList.toggle("show");
+        });
+    }
+});
+
 function showLocation() {
     alert("123 Hair St., Beauty City, Phone: (123) 456-7890");
 }
 
-function confirmAppointment() {
-    window.location.href = 'confirm.html';
-}
+
